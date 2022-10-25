52e8bcb0738558228893c62558489398088fd9bd
Geberlux <116678610+Geberlux@users.noreply.github.com>
2022-10-25 19:17:30 -0300

    Primer cambio
    anvorguesa

---
+++ webanvorguesa/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anvorguesa - Inicio</title>
+</head>
+
+<body>
+    <header>
+        <?php
+           require_once('paginas/header2.php');
+        ?>
+    </header>
+    <footer class="footer">
+       <?php
+            require_once('paginas/footer2.php')
+        ?>
+    </footer>
+
+</body>
+
+</html>
---
+++ webanvorguesa/paginas/hamburguesas.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <title>Hamburguesas</title>
+</head>
+
+<body>
+    <header>
+        <?php
+           require_once('header.php');
+        ?>
+    </header>
+    <footer class="footer">
+       <?php
+            require_once('footer.php')
+        ?>
+    </footer>
+
+</body>
+
+</html>
