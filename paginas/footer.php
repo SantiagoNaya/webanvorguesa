6fa8dd65faee306b1fb9584b0f80ee2f38de7b49
Geberlux <116678610+Geberlux@users.noreply.github.com>
2022-10-25 19:23:37 -0300

    Primer Cambio road to ANVORGUESA FOREVER
    ANVORGUESA BELIEVER

---
+++ paginas/footer.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+
+<div class="content has-text-centered">
+    <p>
+      <strong>Anvorguesa</strong> by <a href="../index.php">Nuestros Nombres</a>
+      <br>
+      &#169; 2022 Anvorguesa Factory
+    </p>
+</div>
---
+++ paginas/header.php
@@ -0,0 +1,62 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+<body>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="../index.php">
+                <img src="../img/gatoanvorguesa.jpg" width="100" height="100">
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="true"
+                data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="../index.php">
+                    Inicio
+                </a>
+
+                <a class="navbar-item" href="hamburguesas.php">
+                    Hamburguesas
+                </a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        Más
+                    </a>
+
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                           Sobre nosotros
+                        </a>
+                      
+                        <a class="navbar-item">
+                            Contacto
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            Reportar un error
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Registrarse</strong>
+                        </a>
+                        <a class="button is-light">
+                            Entrar
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</body>
